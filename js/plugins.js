116b829eb69f5f8d62123619ce78facb0a75c1bf
sorsaffari <soroush@grakn.ai>
2019-04-19 19:25:34 +0100

    complete the sections and refactor html/css

--- js/plugins.js
+++ js/plugins.js
@@ -20,29 +20,3 @@
     }
   }
 }());
-
-// iOS-like scroll behaviour
-$(function (o) {
-  o = $("#venueGallery").overscroll({
-    cancelOn: '.no-drag',
-    captureWheel: false,
-    hoverThumbs: true,
-    persistThumbs: true,
-    showThumbs: false,
-    scrollLeft: 200,
-    scrollTop: 100
-  }).on('overscroll:dragstart overscroll:dragend overscroll:driftstart overscroll:driftend', function (event) {
-    console.log(event.type);
-  });
-});
-
-
-jQuery.fn.overscroll.settings = {
-  captureThreshold:   3,   // The number of mouse move events before considering the gesture a "drag"
-  driftDecay:         1.1, // The linear-friction coefficient for drift decay (must be > 1)
-  driftSequences:     22,  // The number of animation frames to constrain a drift to
-  driftTimeout:       100, // The amount of time to imply the user is no longer trying to drift (in ms)
-  thumbOpacity:       0.7, // The default active opacity for the thumbs
-  thumbThickness:     6,   // The thickness, in pixels, of the generated thumbs
-  thumbTimeout:       400, // The amount of time to wait before fading out thumbs
-}

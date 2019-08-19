96dbd1144803984232097e36380c15cbaaec00c7
Soroush Saffari <soroush@grakn.ai>
2019-08-19 16:52:18 +0100

    add .htaccess with https redirect

--- js/speakersPage.js
+++ js/speakersPage.js
@@ -24,10 +24,9 @@ function generateSpeakerHtml(speaker, profilePicture) {
             <div class="speaker-frame">
                 <img src="${profilePicture.src}" />
             </div>
-            <p class="h5 Titillium-Rg
-                        pt-2">${fullName}</p>
-            <p class="h6 Titillium-Lt pt-1">${position}</p>
-            <a href="${companyUrl}" class="company h6 Titillium-Lt pt-1" style="font-size: 14px;">${company}</a>
+            <p class="fullname h5 Titillium-Rg pt-2">${fullName}</p>
+            <p class="position h6 Titillium-Lt pt-1">${position}</p>
+            <a href="${companyUrl}" target="_blank" class="company h6 Titillium-Lt pt-1">${company}</a>
         </li>
     `;
 }

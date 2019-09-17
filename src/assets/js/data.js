8ed3590f181f7d95da450098c40a524c998eaa50
Soroush Saffari <soroush@grakn.ai>
2019-09-17 17:24:39 +0100

    wip

---
+++ src/assets/js/data.js
@@ -0,0 +1,143 @@
+const getData = async () => {
+  let { speakers, sessions, categories } = await $.get(
+    "https://sessionize.com/api/v2/pixtt19d/view/all"
+  );
+  
+  categories = formatCategories(categories);
+  sessions = formatSessions(sessions, categories);
+  speakers = formatSpeakers(speakers);
+
+  const levels = categories.filter(cat => cat.groupTitle === "Level");
+  const tags = categories.filter(cat => cat.groupTitle === "Tags");
+  
+  return {
+    speakers,
+    sessions,
+    levels,
+    tags
+  };
+};
+
+const collect = (array, current) => array.concat(current);
+
+const formatCategories = categories => {
+  return categories
+    .map(categoriesGroup =>
+      categoriesGroup.items.map(item => {
+        item.groupTitle = categoriesGroup.title;
+        return item;
+      })
+    )
+    .reduce(collect, []);
+};
+
+const formatSessions = (sessions, categories) => {
+  return sessions.map(session => {
+    const {
+      id,
+      title,
+      description,
+      startsAt,
+      endsAt,
+      speakers,
+      categoryItems
+    } = session;
+
+    let level = "";
+    const tags = [];
+    if (categoryItems) {
+      categoryItems.forEach(catItemId => {
+        const catItem = categories.find(cat => cat.id === catItemId);
+        if (catItem.groupTitle === "Level") level = catItem.name;
+        if (catItem.groupTitle === "Tags") tags.push(catItem.name);
+      });
+    }
+
+    const hasMultipleSpeakers = speakers.length > 1;
+
+    return {
+      id,
+      title,
+      description,
+      level,
+      tags,
+      speakers,
+      hasMultipleSpeakers,
+      // apart from start and end time, we also need the day
+      // on which the session takes place i.e. Day 1 or Day 2
+      startsAt,
+      endsAt
+    };
+  });
+};
+
+const formatSpeakers = speakers => {
+  return speakers.map(speaker => {
+    const {
+      id,
+      tagLine,
+      bio,
+      fullName,
+      questionAnswers,
+      profilePicture
+    } = speaker;
+
+    const profileImg = new Image();
+    profileImg.src = profilePicture;
+
+    const companyTitleQId = 16062;
+    const companyTitle = questionAnswers.find(
+      qa => qa.questionId === companyTitleQId
+    ).answerValue;
+
+    const companyUrlQId = 16352;
+    const companyUrl = questionAnswers.find(
+      qa => qa.questionId === companyUrlQId
+    ).answerValue;
+
+    const positionQnId = 16061;
+    const positionShort = questionAnswers.find(
+      qa => qa.questionId === positionQnId
+    ).answerValue;
+    const positionLong = tagLine.split(" at ")[0];
+
+    const companyLogoQId = 16298;
+    const companyLogoFileName = questionAnswers.find(
+      qa => qa.questionId === companyLogoQId
+    ).answerValue;
+
+    const twitterUrlQId = 16350;
+    let twitterUrl = questionAnswers.find(
+      qa => qa.questionId === twitterUrlQId
+    );
+
+    const githubUrlQId = 16349;
+    let githubUrl = questionAnswers.find(qa => qa.questionId === githubUrlQId);
+
+    const linkedinUrlQId = 16351;
+    let linkedinUrl = questionAnswers.find(
+      qa => qa.questionId === linkedinUrlQId
+    );
+
+    return {
+      id,
+      fullName,
+      bio,
+      profileImg,
+      company: {
+        title: companyTitle,
+        url: companyUrl,
+        logo: companyLogoFileName
+      },
+      position: {
+        short: positionShort,
+        long: positionLong
+      },
+      socialLinks: {
+        twitter: twitterUrl,
+        github: githubUrl,
+        linkedin: linkedinUrl
+      }
+    };
+  });
+};
---
+++ src/assets/js/index.js
@@ -0,0 +1,272 @@
+$(document).ready(async function() {
+  const { speakers, sessions } = await getData();
+
+  loadHomeSpeakers(speakers);
+  loadPartners();
+
+  detectInView();
+  scrollToSection();
+
+  const windowWidth = $(window).width();
+  $(window).resize(() => {
+    if ($(window).width() == windowWidth) return; // ignore vertical resize
+    // number of spearkers to show and the swapping behaviour is dependent on the screen size
+    // therefore we call the loadHomeSpeakers again
+    loadHomeSpeakers(speakers, sessions);
+  });
+
+  // common
+  handleSpeakerModalRequest(speakers, sessions);
+  setSpeakerModalHandlers(speakers, sessions);
+  handleHeaderOnScroll();
+  handleSubscription();
+  handleMobileMenu();
+});
+
+const detectInView = () => {
+  const windowWidth = $(window).width();
+
+  if (windowWidth > 500) {
+    const items = document.querySelectorAll(
+        "*[data-animate-in], *[data-detect-viewport]"
+      ),
+      pageOffset = window.pageYOffset;
+
+    function isScrolledIntoView(el) {
+      const rect = el.getBoundingClientRect(),
+        elemTop = rect.top,
+        elemBottom = rect.top + el.offsetHeight,
+        bottomWin = pageOffset + window.innerHeight;
+
+      return elemTop < bottomWin && elemBottom > 0;
+    }
+
+    function detect() {
+      for (let i = 0; i < items.length; i++) {
+        if (isScrolledIntoView(items[i])) {
+          if (!items[i].classList.contains("in-view")) {
+            items[i].classList.add("in-view");
+          }
+        }
+      }
+    }
+
+    window.addEventListener("scroll", detect);
+    window.addEventListener("resize", detect);
+
+    for (let i = 0; i < items.length; i++) {
+      let d = 0,
+        el = items[i];
+
+      if (items[i].getAttribute("data-animate-in-delay")) {
+        d = items[i].getAttribute("data-animate-in-delay") / 1000 + "s";
+      } else {
+        d = 0;
+      }
+      el.style.transitionDelay = d;
+    }
+
+    setTimeout(function() {
+      detect();
+    }, 500);
+  } else {
+    $("[data-animate-in]").removeAttr("data-animate-in");
+  }
+};
+
+const scrollToSection = () => {
+  $("*[data-scroll-to]").on("click touchstart:not(touchmove)", function() {
+    var trigger = $(this).attr("data-scroll-to"),
+      target = $("#" + trigger),
+      ss = 1000, //scroll speed
+      o = $(".site-header").outerHeight(); // offset
+
+    $("body").removeClass("menu-is-open");
+
+    if ($(this).attr("data-scroll-speed")) {
+      ss = $(this).attr("data-scroll-speed");
+    }
+
+    if ($(this).attr("data-scroll-offset")) {
+      o = $(this).attr("data-scroll-offset");
+    }
+
+    $("html, body").animate(
+      {
+        scrollTop: target.offset().top - o
+      },
+      ss
+    );
+  });
+};
+
+const loadHomeSpeakers = speakers => {
+  $("#speakers-home-list").html("");
+  // number of speakers to load and display are determined by the
+  // number of `li`s that are displayed based on the screen size
+  const numOfSpeakers = $("#speakers-list-hidden li").filter(function() {
+    return $(this).css("display") != "none";
+  }).length;
+  const displayedSpeakers = speakers.slice(0, numOfSpeakers);
+  for (const speaker of displayedSpeakers) {
+    $("#speakers-home-list").append(generateSpeakerHtml(speaker));
+    loadSpeakerCompanyLogo(speaker);
+  }
+  swapSpeakers(displayedSpeakers, speakers);
+};
+
+const swapSpeakers = async (displayedSpeakers, allSpeakers) => {
+  const hiddenSpeakers = allSpeakers.filter(
+    speaker => !displayedSpeakers.includes(speaker)
+  );
+
+  const swappingIndexes = {
+    5: [1, 4, 2, 5, 3],
+    4: [1, 3, 2, 4],
+    3: [1, 3, 2],
+    2: [1, 2],
+    1: [1]
+  };
+
+  let shouldSwapSpeakers = true;
+
+  const windowWidth = $(window).width();
+  $(window).resize(() => {
+    if ($(window).width() == windowWidth) return;
+    shouldSwapSpeakers = false;
+  });
+
+  await new Promise(done => setTimeout(() => done(), 2000));
+
+  while (shouldSwapSpeakers) {
+    for (let i of swappingIndexes[displayedSpeakers.length]) {
+      // the window hasn't been resized yet
+      if (shouldSwapSpeakers) {
+        const swappingSpeed = 500;
+        const intervalDuration = (1 / displayedSpeakers.length) * 1000;
+        await new Promise(done => setTimeout(() => done(), intervalDuration));
+
+        const speakerToHideEl = $(
+          "#speakers-home-list li:nth-child(" + i + ")"
+        );
+        const idOfSpeakerToHide = speakerToHideEl.data("speaker-id");
+        const speakerToHide = allSpeakers.find(
+          speaker => speaker.id === idOfSpeakerToHide
+        );
+
+        const nextSpeaker = hiddenSpeakers[0];
+        const nextSpeakerEl = $(generateSpeakerHtml(nextSpeaker));
+        nextSpeakerEl.hide();
+
+        speakerToHideEl.fadeOut(swappingSpeed, () => {
+          speakerToHideEl.replaceWith(nextSpeakerEl);
+          loadSpeakerCompanyLogo(nextSpeaker);
+          nextSpeakerEl.fadeIn(swappingSpeed);
+        });
+
+        await new Promise(done => setTimeout(() => done(), swappingSpeed * 2));
+
+        displayedSpeakers[i - 1] = nextSpeaker;
+        hiddenSpeakers.shift();
+        hiddenSpeakers.push(speakerToHide);
+      }
+    }
+  }
+};
+
+const loadPartners = () => {
+  const partners = [
+    {
+      image: "taxfix.png",
+      url: "https://taxfix.de"
+    },
+    {
+      image: "icelab.png",
+      url: "http://icelab.uk"
+    },
+    {
+      image: "sourcepp.png",
+      url: "https://sourceplusplus.com"
+    },
+    {
+      image: "ariwonto.png",
+      url: "https://ariwonto.com"
+    },
+    {
+      image: "mauna.png",
+      url: "https://www.mauna.ai"
+    },
+    {
+      image: "deutschetelekom.png",
+      url: "https://www.telekom.com"
+    },
+    {
+      image: "austincapitaldata.png",
+      url: "https://capitaldata.github.io"
+    },
+    {
+      image: "google.png",
+      url: "https://cloud.google.com"
+    },
+    {
+      image: "augmentedthinking.png",
+      url: "https://www.augmented-thinking.ai"
+    },
+    {
+      image: "tno.png",
+      url: "https://www.tno.nl"
+    },
+    {
+      image: "yooi.png",
+      url: "https://www.yooi.com"
+    },
+    {
+      image: "infosys.png",
+      url: "https://www.infosys.com"
+    },
+    {
+      image: "astrazeneca.png",
+      url: "https://www.astrazeneca.co.uk"
+    },
+    {
+      image: "opencti.png",
+      url: "https://www.opencti.io"
+    },
+    {
+      image: "aresgenetics.png",
+      url: "https://www.ares-genetics.com"
+    },
+    {
+      image: "ustglobal.png",
+      url: "https://ust-global.com"
+    },
+    {
+      image: "dunnhumby.png",
+      url: "https://www.dunnhumby.com"
+    },
+    {
+      image: "rasa.png",
+      url: "https://rasa.com"
+    },
+    {
+      image: "cisco.png",
+      url: "https://cisco.com"
+    },
+    {
+      image: "icarcnr.png",
+      url: "https://www.icar.cnr.it/en/"
+    }
+  ];
+
+  for (partner of partners) {
+    partnerHtml = `
+            <div class="partner-logo w-sm-33 w-lg-25 w-lgc-20 p-8 p-xs-3 p-sm-6 p-md-4 p-lgc-6">
+                <a href="${partner.url}" target="_blank">
+                    <img class="mx-auto" src="/img/companies/${partner.image}" />
+                </a>
+            </div>
+        `;
+
+    $("#partners-list").append(partnerHtml);
+  }
+};
